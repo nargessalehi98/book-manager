859ce5761b4be000f338e4dec5f8f7ef442119dd
narges <nargessalehi98@aut.ac.ir>
2023-05-11 16:56:40 +0330

    Update models and migrations.

--- manager/models.py
+++ manager/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
+
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     publication_date = models.DateField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
+
     cover_image_url = models.URLField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     num_pages = models.PositiveIntegerField(blank=True, null=True)
-    subjects = models.TextField(blank=True, null=True)
+    subjects = models.CharField(blank=True, max_length=1000)
+    isbn = models.CharField(max_length=13, primary_key=True)
+
+    def set_subject_list(self, value):
+        self.my_list = ','.join(value)
+
+    def get_subject_list(self):
+        return self.my_list.split(',')
