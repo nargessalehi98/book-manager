2ff57b377f233f28a0d4639b4071466a8025f682
narges <nargessalehi98@aut.ac.ir>
2023-05-11 21:16:03 +0330

    Remove ISBN field from Book Model.

--- manager/models.py
+++ manager/models.py
@@ -6,12 +6,10 @@ class Book(models.Model):
     author = models.CharField(max_length=255)
     publication_date = models.DateField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
-
     cover_image_url = models.URLField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     num_pages = models.PositiveIntegerField(blank=True, null=True)
     subjects = models.CharField(blank=True, max_length=1000)
-    isbn = models.CharField(max_length=13, primary_key=True)
 
     def set_subject_list(self, value):
         self.my_list = ','.join(value)
