5bfb583a30a78e8a4c40e6d0a5a78c0016de679a
narges <nargessalehi98@aut.ac.ir>
2023-05-12 16:44:00 +0330

    Add new field and methods to Book Model.

--- manager/models.py
+++ manager/models.py
@@ -10,9 +10,24 @@ class Book(models.Model):
     description = models.TextField(blank=True, null=True)
     num_pages = models.PositiveIntegerField(blank=True, null=True)
     subjects = models.CharField(blank=True, max_length=1000)
+    updated = models.BooleanField(default=False)
+
+    def is_updated(self):
+        return self.updated
 
     def set_subject_list(self, value):
-        self.my_list = ','.join(value)
+        self.my_list = ','.join(value) if value else None
 
     def get_subject_list(self):
         return self.my_list.split(',')
+
+    def create_title_query(self):
+        return self.title.replace(" ", "+")
+
+    def update_extra_info(self, num_pages, subjects, description, cover_imager_url):
+        self.num_pages = num_pages
+        self.set_subject_list(subjects)
+        self.description = description
+        self.cover_image_url = cover_imager_url
+        self.updated = True
+        self.save()
