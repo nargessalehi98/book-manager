1d4f454a071b6617de0f8a45e1a7b2200c30437e
narges <nargessalehi98@aut.ac.ir>
2023-05-12 17:17:28 +0330

    Overwrite to_representation in BookSerializer in order to download cover image.

--- manager/serializer.py
+++ manager/serializer.py
@@ -1,3 +1,4 @@
+import requests
 from rest_framework import serializers
 from .models import Book
 
@@ -6,6 +7,15 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ('title', 'author', 'publication_date', 'price', 'cover_image_url')
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        file_url = data.get('cover_image_url')
+        if file_url:
+            response = requests.get(file_url)
+            if response.status_code == 200:
+                data['file'] = response.content.decode(encoding='ISO-8859-1')
+        return data
 
 
 class SpecificBookSerializer(serializers.ModelSerializer):
